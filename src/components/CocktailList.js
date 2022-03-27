85920231b3d390fbba2208c3d933f08cca145ed9
sylvestre <pasytchangwa@gmail.com>
2022-03-27 18:30:13 +0300

    Add concktail list component

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
+import Loading from './Loading'
 
 const CocktailList = () => {
+  const {cocktails, isLoading} = useGlobalContext()
+
+  if (isLoading) {
+    return <div>
+      <Loading />
+    </div>
+  }
   return (
     <div>
       
