d1191d617b9c705e851dc33bd4eeb85b17d1f41c
sylvestre <pasytchangwa@gmail.com>
2022-03-27 17:47:29 +0300

    Add Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
     <div>
      <h2>404 Error</h2>
      <h2>No page found</h2>
+     <Link to="/">Go back to home</Link>
     </div>
   )
 }
