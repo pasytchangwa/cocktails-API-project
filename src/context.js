f6e31998d83e54d09ed87f071fc6e0df22aa2c1a
sylvestre <pasytchangwa@gmail.com>
2022-03-27 17:47:53 +0300

    Add globalcontext

--- src/context.js
+++ src/context.js
@@ -1,12 +1,29 @@
-import React, {useContext, createContext} from "react";
+import React, {useState, useContext, createContext} from "react";
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 const AppContext = createContext();
 
 const AppProvider = ({children}) => {
+  const [cocktails, setCocktails] = useState([]);
+  const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchCocktails = async () => {
+   try {
+     setIsLoading(true);
+     const response = await fetch(url + search);
+     const data = await response.json();
+     console.log(data)
+     setCocktails(data.drinks);
+     setIsLoading(false);
+   } catch (error) {
+     console.log(error);
+   }
+  }
 
 
   return (
-    <AppContext.Provider value={{}}>
+    <AppContext.Provider value={{cocktails,search,isLoading}}>
       {children}
     </AppContext.Provider>
   );
