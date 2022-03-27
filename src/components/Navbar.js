9359726f4bf5aaefae5aa2f996a955b797496b76
sylvestre <pasytchangwa@gmail.com>
2022-03-27 17:48:14 +0300

    Add Navbar page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from './logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <div>
+        <link to='/'>
+          <img src={logo} alt='logo' />
+        </link>
+      </div>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
