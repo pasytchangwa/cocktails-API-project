d16fecbd1aee9470c7268fbdc3929f2fff3b319d
sylvestre <pasytchangwa@gmail.com>
2022-03-28 19:45:18 +0300

    Update Eror page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-const Error = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default function Error() {
   return (
     <section className='error-page section'>
-      <div className="error-container">
-        <h2>page not found</h2>
-        <Link to="/" className='btn btn-primary'>Back to Homepage</Link>
+      <div className='error-container'>
+        <h1>oops! it's a dead end</h1>
+        <Link to='/' className='btn btn-primary'>
+          back home
+        </Link>
       </div>
     </section>
-  )
+  );
 }
-
-export default Error
