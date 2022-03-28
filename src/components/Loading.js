568e8c31ba3f5f741c0c313b56c35c1e96c04cc8
sylvestre <pasytchangwa@gmail.com>
2022-03-28 19:47:27 +0300

    Add loading file

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Loading = () => {
-  return (
-    <div className='loader' />
-  )
-}
+  return <div className='loader'></div>;
+};
 
-export default Loading
+export default Loading;
