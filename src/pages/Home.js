fc7e9277afc2e24cd4c38164dbac8192667a6157
sylvestre <pasytchangwa@gmail.com>
2022-03-28 19:44:59 +0300

    Add Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,11 @@
-import React from 'react'
-import SearchForm from '../components/SearchForm'
-import CocktailList from '../components/CocktailList'
-
-const Home = () => {
+import React from 'react';
+import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
+export default function Home() {
   return (
     <main>
       <SearchForm />
       <CocktailList />
     </main>
-  )
+  );
 }
-
-export default Home
