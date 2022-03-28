a17e76b1deef3c90e22efa2320b3ce4b7a190f96
sylvestre <pasytchangwa@gmail.com>
2022-03-28 19:44:39 +0300

    Add SingleCocktail page

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Loading from '../components/Loading';
+import { useParams, Link } from 'react-router-dom';
 
-const SingleCocktail = () => {
-  return (
-    <div>SingleCocktail</div>
-  )
-}
+export default function SingleCocktail() {
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
 
-export default SingleCocktail
+  React.useEffect(() => {
+    setLoading(true);
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setCocktail(data.drinks[0]);
+        setLoading(false);
+      });
+  }
